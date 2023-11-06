4192b8cfcf777e91817159d84815d6480771c133
pablo pernías <pablo@pernias.com>
2023-11-07 00:39:27 +0100

    add Sqrt schedules

--- gdf/schedulers.py
+++ gdf/schedulers.py
@@ -41,6 +41,25 @@ class CosineTrainSchedule2(CosineSampleSchedule2):
     def schedule(self, batch_size):
         t = 1-torch.rand(batch_size)
         return super().schedule(t)
+    
+class CosineTrainSchedule(CosineSampleSchedule):
+    def schedule(self, batch_size):
+        t = (1-torch.rand(batch_size)).add(0.001).clamp(0.001, 1.0)
+        return super().schedule(t)
+    
+class SqrtSampleSchedule(BaseSchedule):
+    def __init__(self, s=1e-4):
+        self.s = s
+
+    def schedule(self, t):
+        var = 1 - (t + self.s)**0.5
+        logSNR = (var/(1-var)).log()
+        return logSNR
+    
+class SqrtTrainSchedule(SqrtSampleSchedule):
+    def schedule(self, batch_size):
+        t = 1-torch.rand(batch_size)
+        return super().schedule(t)
 
 class RectifiedFlowsSampleSchedule(BaseSchedule):
     def __init__(self, logsnr_range=[-15, 15]):
