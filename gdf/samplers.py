26d0c4e37dded5e8ec3a3c3711946076e815b266
pablo pernías <pablo@pernias.com>
2023-11-19 01:11:51 +0100

    Add LCM Sampler

--- gdf/samplers.py
+++ gdf/samplers.py
@@ -32,3 +32,9 @@ class DDPMSampler(DDIMSampler):
     def step(self, x, x0, epsilon, logSNR, logSNR_prev, eta=1):
         return super().step(x, x0, epsilon, logSNR, logSNR_prev, eta)
     
+class LCMSampler(SimpleSampler):
+    def step(self, x, x0, epsilon, logSNR, logSNR_prev):        
+        a_prev, b_prev = self.gdf.input_scaler(logSNR_prev)
+        a_prev, b_prev = a_prev.view(-1, *[1]*(len(x0.shape)-1)), b_prev.view(-1, *[1]*(len(x0.shape)-1))
+        return x0 * a_prev + torch.randn_like(epsilon) * b_prev
+    
