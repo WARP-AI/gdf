014a824a4799cbe98a6ebab676bdc6d258a15877
pablo pernías <pablo@pernias.com>
2023-11-24 12:12:29 +0100

    a bit of refactor

--- gdf/scalers.py
+++ gdf/scalers.py
@@ -1,9 +1,6 @@
-def BaseScaler():
+class BaseScaler():
     def __init__(self):
         self.adjusted_limits = None
-
-    def __call__(self, logSNR):
-        raise Exception("this method needs to be overriden")
 
     def setup_limits(self, min_a, max_a, min_b, max_b):
         self.adjusted_limits = min_a, max_a, min_b, max_b
@@ -11,21 +8,26 @@ def BaseScaler():
     def adjust_limits(self, a, b):
         min_a, max_a, min_b, max_b = self.adjusted_limits
         return a - min_a / (max_a - min_a), b - min_b / (max_b - min_b)
-
-class VPScaler(BaseScaler):
-    def __call__(self, logSNR): 
-        a_squared = logSNR.sigmoid()
-        a = a_squared.sqrt()
-        b = (1-a_squared).sqrt()
+    
+    def scalers(self, logSNR):
+        raise Exception("this method needs to be overriden")
+    
+    def __call__(self, logSNR):
+        a, b = self.scalers(logSNR)
         if self.adjusted_limits is not None:
             a, b = self.adjust_limits(a, b)
         return a, b
+
+class VPScaler(BaseScaler):
+    def scalers(self, logSNR): 
+        a_squared = logSNR.sigmoid()
+        a = a_squared.sqrt()
+        b = (1-a_squared).sqrt()
+        return a, b
     
 class LERPScaler(BaseScaler):
-    def __call__(self, logSNR):
+    def scalers(self, logSNR):
         _a = logSNR.exp() - 1
         a = 1 + (2-(2**2 + 4*_a)**0.5) / (2*_a)
         b = (1-a)
-        if self.adjusted_limits is not None:
-            a, b = self.adjust_limits(a, b)
         return a, b
